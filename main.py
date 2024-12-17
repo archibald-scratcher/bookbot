23c493d7e4234ef99af35d4b3be30ff6cdf511cb
PRINCZ-Akos <10927744+PRINCZ-Akos@users.noreply.github.com>
2024-12-17 01:40:45 +0100

    count characters

--- main.py
+++ main.py
@@ -9,4 +9,14 @@ def count_words(text):
         count += 1
     return count
 
+def count_characters(text):
+    character_counts = {}
+    for character in text:
+        character = character.lower()
+        if character in character_counts:
+            character_counts[character] += 1
+        else:
+            character_counts[character] = 1
+    return character_counts
+
 main()
