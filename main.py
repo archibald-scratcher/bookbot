92612bd289702f7b5036570f16744719e2886df0
PRINCZ-Akos <10927744+PRINCZ-Akos@users.noreply.github.com>
2024-12-17 01:39:39 +0100

    count words

--- main.py
+++ main.py
@@ -3,4 +3,10 @@ def main():
     with open(path_to_file) as f:
         file_contents = f.read()
 
+def count_words(text):
+    count = 0
+    for _ in text.split():
+        count += 1
+    return count
+
 main()
